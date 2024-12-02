f54ded26cf756d06a74a33038f8c0529bab7058f
YoshinoNao <113161290+Yoshino-Nao@users.noreply.github.com>
2024-12-02 12:28:38 +0900

    ファイル読み込みテスト

--- counter.js
+++ counter.js
@@ -1,20 +1,9 @@
-const number = document.querySelector("p");
-const countButton = document.querySelector("button");
-
-window.addEventListener("load", () => {
-  var jsonCount = localStorage.getItem("storage");
-  count = JSON.parse(jsonCount);
-
-  if (!count) {
-    number.innerHTML = 0;
-  } else {
-    number.innerHTML = count;
-  }
-});
-countButton.addEventListener("click", function () {
-  count++;
-  number.innerHTML = count;
-
-  jsonCount = JSON.stringify(count);
-  localStorage.setItem("storage", jsonCount);
-});
+fetch("count.txt")
+  .then((response) => response.text())
+  .then((data) => {
+    console.log("テキストファイルの内容:");
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("エラー:", error);
+  });
